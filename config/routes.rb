4882f0aa4ecac3fb55048ac0f019d157fa74e7d6
Bode Fuwa <bodefuwa@gmail.com>
2015-07-09 12:52:53 -0400

    added toggle button

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/terms'
-  get 'pages/privacy'
-  get 'pages/faqs'
-  get 'pages/help'
+  # get => 'pages/home'
+  get 'about'   => 'pages/about'
+  get 'contact' => 'pages/contact'
+  get 'terms'   => 'pages/terms'
+  get 'privacy' => 'pages/privacy'
+  get 'faqs'    => 'pages/faqs'
+  get 'help'    => 'pages/help'
 
   resources :resources
   resources :resource_types
   resources :professional_levels
   resources :users
-  root 'users#index'
+  root 'pages#home'
 end
